f7a32f4dc76a0e7d21d97ed130729379f63bc2c7
Rüdiger zu Dohna <snackbox@sinntr.eu>
2018-02-24 09:05:04 +0100

    more tests

--- src/test/java/XmlTest.java
+++ src/test/java/XmlTest.java
@@ -1,32 +1,46 @@
 import com.github.t1.xml.Xml;
-import org.junit.Ignore;
+import lombok.val;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
 
 public class XmlTest {
-    private static final String XML = "" +
-            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<root>\n" +
-            "    <!-- my-comment -->\n" +
-            "</root>\n";
+    private static final URL URL = XmlTest.class.getResource("sample.xml");
+    private static final String XML = contentOf(URL);
     private final Xml xml = build();
 
     private Xml build() {
-        Xml xml = Xml.createWithRootElement("root");
+        val xml = Xml.createWithRootElement("root");
         xml.addComment("my-comment");
+        xml.getOrCreateElement("elem")
+                .setAttribute("attr", "val")
+                .addText("foo")
+                .addText("bar");
         return xml;
     }
 
     @Test
     public void shouldHaveToXmlString() {
-        assertEquals(XML, xml.toXmlString());
+        assertThat(xml.toXmlString()).isEqualTo(XML);
     }
 
     @Test
-    @Ignore
     public void shouldBeEqualToFromString() {
         Xml actual = Xml.fromString(XML);
-        assertEquals(xml, actual);
+        assertThat(actual).hasToString(xml.toString());
+    }
+
+    @Test
+    public void shouldLoadFromFile() throws URISyntaxException {
+        val actual = Xml.load(URL.toURI());
+
+        assertThat(actual.toXmlString()).isEqualTo(XML);
+        assertThat(actual.uri()).isEqualTo(URL.toURI());
+        assertThat(actual.uri().toString())
+                .isEqualTo("file:" + System.getProperty("user.dir") + "/target/test-classes/sample.xml");
     }
 }
