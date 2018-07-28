c85d87a194ab56f1d5b569bbd6a2253e25a712a6
Rüdiger zu Dohna <snackbox@sinntr.eu>
2018-07-28 17:25:24 +0200

    more from pomx; updated dependencies; proper use of junit 5

--- src/test/java/XmlTest.java
+++ src/test/java/XmlTest.java
@@ -1,6 +1,6 @@
 import com.github.t1.xml.Xml;
 import lombok.val;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -8,7 +8,7 @@ import java.net.URL;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.contentOf;
 
-public class XmlTest {
+class XmlTest {
     private static final URL URL = XmlTest.class.getResource("sample.xml");
     private static final String XML = contentOf(URL);
     private final Xml xml = build();
@@ -23,19 +23,16 @@ public class XmlTest {
         return xml;
     }
 
-    @Test
-    public void shouldHaveToXmlString() {
+    @Test void shouldHaveToXmlString() {
         assertThat(xml.toXmlString()).isEqualTo(XML);
     }
 
-    @Test
-    public void shouldBeEqualToFromString() {
+    @Test void shouldBeEqualToFromString() {
         Xml actual = Xml.fromString(XML);
         assertThat(actual).hasToString(xml.toString());
     }
 
-    @Test
-    public void shouldLoadFromFile() throws URISyntaxException {
+    @Test void shouldLoadFromFile() throws URISyntaxException {
         val actual = Xml.load(URL.toURI());
 
         assertThat(actual.toXmlString()).isEqualTo(XML);
